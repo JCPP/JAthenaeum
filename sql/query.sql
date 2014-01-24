4accf12711d8fe91e853b0d3ef6b1f8babdf3194
DavidePastore <pasdavide@gmail.com>
2014-01-24 02:55:21 +0100

    Add query to create the MySql database.
    Closed #3.

---
+++ sql/query.sql
@@ -0,0 +1,63 @@
+/* Create database */
+CREATE DATABASE JAthenaeum;
+
+USE JAthenaeum;
+
+/* Create tables */
+
+
+/* Create table Utente */
+CREATE TABLE Utente (
+	IDUtente integer AUTO_INCREMENT PRIMARY KEY,
+	EmailUtente varchar(50) NOT NULL,
+	PasswordUtente varchar(50) NOT NULL,
+	NomeUtente varchar(50),
+	CognomeUtente varchar(50),
+	DataNascitaUtente date,
+	NumeroTesseraUtente integer
+);
+
+
+/* Create table Libro */
+CREATE TABLE Libro (
+	IDLibro integer AUTO_INCREMENT PRIMARY KEY,
+	TitoloLibro varchar(50) NOT NULL,
+	CopertinaLibro varchar(200),
+	GenereLibro varchar(50),
+	CodiceIsbnLibro varchar(13),
+	DescrizioneLibro varchar(500)
+);
+
+
+/* Create table Prestito */
+CREATE TABLE Prestito (
+	IDPrestito integer AUTO_INCREMENT PRIMARY KEY,
+	IDUtente integer NOT NULL,
+	IDLibro integer NOT NULL,
+	DataInizioPrestito date NOT NULL,
+	DataFinePrestito date NOT NULL,
+	FOREIGN KEY (IDUtente) REFERENCES Utente(IDUtente),
+	FOREIGN KEY (IDLibro) REFERENCES Libro(IDLibro)
+);
+
+
+/* Create table Autore */
+CREATE TABLE Autore (
+	IDAutore integer AUTO_INCREMENT PRIMARY KEY,
+	NomeAutore varchar(50) NOT NULL,
+	CognomeAutore varchar(50) NOT NULL,
+	FotoAutore varchar(200),
+	DataNascitaAutore date,
+	BiografiaAutore varchar(1000)
+);
+
+
+/* Create table Scrivere */
+CREATE TABLE Scrivere (
+	IDScrivere integer AUTO_INCREMENT PRIMARY KEY,
+	IDLibro integer NOT NULL,
+	IDAutore integer NOT NULL,
+	FOREIGN KEY (IDLibro) REFERENCES Libro(IDLibro),
+	FOREIGN KEY (IDAutore) REFERENCES Autore(IDAutore)
+);
+
